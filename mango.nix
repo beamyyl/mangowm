d3402cb8fe6813d9d7b04a96250cfc047f72df18
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-03 21:39:08 +0300

    Create mango.nix

---
+++ mango.nix
@@ -0,0 +1,45 @@
+{ lib, stdenv, meson, ninja, pkg-config, wayland-protocols, wayland-scanner, libxkbcommon, wayland, pixman, libxcb, pcre2, libxcb-wm, libx11, wlroots_0_19, scenefx, libinput, mesa, seatd, cjson, libdrm, libdisplay-info, libliftoff, xwayland, libGL }:
+
+stdenv.mkDerivation rec {
+  pname = "mango";
+  version = "local-fork";
+  src = ./. + "";
+
+  nativeBuildInputs = [ 
+    meson 
+    ninja 
+    pkg-config 
+    wayland-protocols 
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    libxkbcommon
+    wayland
+    pixman
+    libxcb
+    pcre2
+    libxcb-wm
+    libx11
+    wlroots_0_19
+    scenefx
+    libinput
+    mesa
+    seatd
+    cjson
+    libdrm
+    libdisplay-info
+    libliftoff
+    xwayland
+    libGL
+  ];
+
+  passthru.providedSessions = [ "mango" ];
+
+  meta = with lib; {
+    description = "Practical and Powerful wayland compositor";
+    homepage = "https://github.com/beamyyl/mangowm";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+  };
+}
